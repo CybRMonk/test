3400b0128629a12e7dfc14baf50c68ccadeb52e8
Daniel <danmena952@gmail.com>
2016-02-18 18:04:39 -0600

    Elephants love me

---
+++ com/DanielMena/Dinosaur.java
@@ -0,0 +1,11 @@
+package com.DanielMena;
+
+/**
+ * Created by Daniel on 2/18/2016.
+ */
+public class Dinosaur {
+
+    public void roar() {
+        System.out.println("GRRRR!!!");
+    }
+}
---
+++ com/DanielMena/DinosaurSupervisor.java
@@ -0,0 +1,21 @@
+package com.DanielMena; /**
+ * Created by Daniel on 2/18/2016.
+ */
+
+    /**
+     * Created by Daniel on 2/18/2016.
+     */
+    public class DinosaurSupervisor {
+
+        public static void main(String[] args) {
+
+            Dinosaur t_rex = new Dinosaur();
+            Dinosaur velociraptor = new Dinosaur();
+
+            t_rex.roar();
+            velociraptor.roar();
+        }
+
+
+    }
+
